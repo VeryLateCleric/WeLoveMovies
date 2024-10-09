1b362cd53199f8d54c12829ce192975ae5b4e3be
Kenneth <kennethyoung.2960@gmail.com>
2024-10-09 03:48:05 -0700

    Added movies controller and service functions.

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,32 +1,60 @@
 const moviesService = require("./movies.service");
-const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-// List all movies, and filter is 'is_showing' query added
-async function list(req, res) {
-  const { is_showing } = req.query; //Get query param
-  let data;
-  if (is_showing === "true") {
-    // Filter only active movies
-    data = await moviesService.listActiveMovies();
-  } else {
-    // Show all
-    data = await moviesService.listAll();
+async function checkMovieExists(req, res, next) {
+  const movie = await moviesService.read(req.params.movieId);
+  if (movie) {
+    res.locals.movie = movie;
+    return next();
   }
-  res.json({ data });
+  next({ status: 404, message: "Movie cannot be found." });
 }
 
 // Read movie details by given ID
 async function read(req, res, next) {
   const { movieId } = req.params;
   const movie = await moviesService.read(movieId);
-  if (!movie) {
-    // Fail first
+  if (!movie) { // Fail first
     return next({ status: 404, message: `Movie cannot be found` });
   }
   res.json({ data: movie });
 }
 
+// List all movies, and filter if 'is_showing' query added
+async function listMoviesShowing(req, res) {
+  const { is_showing } = req.query; //Get query param
+  let data;
+  if (is_showing === "true") { // Filter only active movies
+    data = await moviesService.listActiveMovies();
+  } else { // Show all
+    data = await moviesService.listAllMovies();
+  }
+  res.json({ data });
+}
+
+// List of theaters playing the requested movie
+async function listTheatersPlayingMovie(req, res) {
+  const { movieId } = req.params;
+  const data = await moviesService.listTheatersPlayingMovie(movieId);
+  res.json({ data });
+}
+
+// Show reviews associated with each movie's id
+async function listMoviesReviews(req, res) {
+  const { movieId } = req.params;
+  const data = await moviesService.listReviewsWithCritics(movieId);
+  res.json({ data });
+}
+
 module.exports = {
-  list: asyncErrorBoundary(list),
-  read: asyncErrorBoundary(read),
+  list: asyncErrorBoundary(listMoviesShowing),
+  read: [asyncErrorBoundary(checkMovieExists), read],
+  listTheatersPlaying: [
+    asyncErrorBoundary(checkMovieExists),
+    asyncErrorBoundary(listTheatersPlayingMovie),
+  ],
+  listMoviesReviews: [
+    asyncErrorBoundary(checkMovieExists),
+    asyncErrorBoundary(listMoviesReviews)
+  ]
 };
---
+++ src/movies/movies.service.js
@@ -0,0 +1,59 @@
+const knex = require("../db/connection");
+const mapProperties = require("../utils/map-properties");
+
+// Get all movies
+function listAllMovies() {
+  return knex("movies");
+}
+
+// Get movies where is_showing is true
+function listMoviesPlaying() {
+  return knex(
+    "movies as m"
+      .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")
+      .distinct("m.*")
+      .where({ "mt.is_showing": true })
+  );
+}
+
+function read(movieId) {
+  return knex("movies as m").select("*").where({ movie_id: movieId }).first();
+}
+
+// join tables theaters and movies_theaters
+function listTheatersPlayingMovie(movieId) {
+  return knex("theaters as t")
+    .join("movies_theaters as mt", "t.theaters_id", "mt.theater_id")
+    .select("*")
+    .where({ "mt.movie_id": movieId });
+}
+
+const addCritic = mapProperties({
+  critic_id: "critic.critic_id",
+  preferred_name: "critic.preferred_name",
+  surname: "critic.surname",
+  organization_name: "critic.organization_name",
+  created_at: "critic.created_at",
+  updated_at: "critic.updated_at",
+});
+
+// join tables reviews and critics
+function listReviewsWithCritics(movieId) {
+  return knex("reviews as r")
+    .join("critics as c", "c.critic_id", "r.critic_id")
+    .select("*")
+    .where({ "r.movie_id": movieId })
+    .then((data) =>
+      data.map((review) => {
+        return addCritic(review);
+      })
+    );
+}
+
+module.exports = {
+  listAllMovies,
+  listMoviesPlaying,
+  read,
+  listTheatersPlayingMovie,
+  listReviewsWithCritics
+};
